339784a3996d782afcc690910e19704e1732527d
LuisNeil <lntejedal@gmail.com>
2022-02-11 08:17:03 -0500

    Actualizacion hasta formato fecha

--- src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -1,34 +1,55 @@
 package com.bolsadeideas.springboot.form.app.controller;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
+
+    @Autowired
+    private UsuarioValidador validador;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        binder.addValidators(validador);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, "fechaNacimiento", new CustomDateEditor(dateFormat,true));
+    }
 
     @GetMapping("/form")
     public String form(Model model){
         Usuario usuario = new Usuario();
+        usuario.setNombre("John");
+        usuario.setApellido("Doe");
+        usuario.setIdentificador("11.001.101-L");
         model.addAttribute("titulo","Formulario usuarios");
         model.addAttribute(usuario);
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status
                            /*@RequestParam String username,
                            @RequestParam String password,
                            @RequestParam String email*/){
+        //validador.validate(usuario,result);
 
         /*Usuario usuario = new Usuario();
         usuario.setUsername(username);
@@ -48,6 +69,7 @@ public class FormController {
 
         model.addAttribute("titulo","Resultado form");
         model.addAttribute("usuario",usuario);
+        status.setComplete();
         return "resultado";
     }
 
--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,17 +1,68 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
-import javax.validation.constraints.NotEmpty;
+import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
+import com.bolsadeideas.springboot.form.app.validation.Requerido;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.util.Date;
 
 public class Usuario {
 
-    @NotEmpty
+    //@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+    @IdentificadorRegex
+    private String identificador;
+
+    //@NotEmpty(message = "The name can not be empty")
+    private String nombre;
+
+    //@NotEmpty
+    @Requerido
+    private String apellido;
+
+    @NotBlank
+    @Size(min = 3, max = 8)
     private String username;
 
     @NotEmpty
     private String password;
 
     @NotEmpty
+    @Email
     private String email;
+
+    @NotNull
+    @Min(5)
+    @Max(5000)
+    private Integer cuenta;
+
+    @NotNull
+    //@DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
     public String getUsername() {
         return username;
@@ -35,5 +86,21 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Integer getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Integer cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 }
